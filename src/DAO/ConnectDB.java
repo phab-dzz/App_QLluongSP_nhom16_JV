9bf6568270c5cf81196afab4f8aefac2df016d77
Phandz <duongvanphan@gmail.com>
2023-10-21 21:06:01 +0700

    add data sql

---
+++ src/DAO/ConnectDB.java
@@ -0,0 +1,43 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class ConnectDB {
+	public static Connection con=null;
+	private static ConnectDB instance= new ConnectDB();
+	public  void connect() throws SQLException {
+		String url="jdbc:sqlserver://localhost:1433;databasename=QLPB";
+		con= DriverManager.getConnection(url,"sa","password");
+		
+		
+	}
+	public void disconnect() throws SQLException {
+		if(con!=null) {
+			con.close();
+		}
+	}
+	public static ConnectDB getInstance() {
+		return instance;
+	}
+	public static Connection getConnection() {
+//		System.out.println("success");
+		return con;
+	}
+	public static void main(String[] args) throws SQLException {
+//		ConnectDB connect = new ConnectDB();
+//		ConnectDB.getInstance();
+		Connection c= null;
+		String url="jdbc:sqlserver://localhost:1433;databasename=QLLUONG";
+		c= DriverManager.getConnection(url,"sa","password");
+		System.out.println(c);
+//		
+//		if(c== null)
+//			System.out.println("failed");
+//		else
+//			System.out.println("success");
+	}
+
+}
