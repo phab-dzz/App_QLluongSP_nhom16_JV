f573902311d719e300fb40ce8b619a6543f724c3
TrinhNamTrung <trung@gmail.com>
2023-10-27 22:04:12 +0700

    Update create GUI NhanSu

--- src/Dangnhap/Dangnhap.java
+++ src/Dangnhap/Dangnhap.java
@@ -5,6 +5,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.SoftBevelBorder;
 
 import App_QLLuong.App_main;
 
@@ -13,6 +14,7 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
@@ -33,14 +35,14 @@ import javax.swing.JCheckBox;
 import javax.swing.border.BevelBorder;
 import javax.swing.ImageIcon;
 
-public class Dangnhap extends JFrame implements ActionListener{
+public class Dangnhap extends JFrame implements ActionListener {
 
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField txtma;
-	 private JButton btndn;
-	 private JPasswordField pass;
-	 private JCheckBox jcbxem;
-	 private 	JLabel lblexit;
+	private ImageIcon ImageLogo;
+	private JTextField textFieldUsername;
+	private JPasswordField passwordField;
+	private JButton btnLogin;
 
 	/**
 	 * Launch the application.
@@ -62,143 +64,156 @@ public class Dangnhap extends JFrame implements ActionListener{
 	 * Create the frame.
 	 */
 	public Dangnhap() {
-		setTitle("Đăng nhập hệ thống quản lý lương");
-//		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-	       
-        this.setUndecorated(true);
-		setBounds(100, 100, 450, 300);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 764, 469);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JPanel dangnhap = new JPanel();
-		dangnhap.setBounds(0, 0, 450, 300);
-		contentPane.add(dangnhap);
-		dangnhap.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("Đăng Nhập tài khoản");
-		lblNewLabel.setForeground(new Color(244, 164, 96));
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 25));
-		lblNewLabel.setBounds(52, 11, 304, 60);
-		dangnhap.add(lblNewLabel);
-		
-		txtma = new JTextField();
-		txtma.setBounds(155, 82, 213, 34);
-		dangnhap.add(txtma);
-		txtma.setColumns(10);
-		
-		JLabel lblNewLabel_1 = new JLabel("Mã số Nhân viên:");
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setFont(new Font("Times New Roman", Font.BOLD, 14));
-		lblNewLabel_1.setBounds(20, 82, 125, 34);
-		dangnhap.add(lblNewLabel_1);
-		
-		JLabel lblNewLabel_1_1 = new JLabel(" Mật khẩu :");
-		lblNewLabel_1_1.setHorizontalAlignment(SwingConstants.LEFT);
-		lblNewLabel_1_1.setFont(new Font("Times New Roman", Font.BOLD, 14));
-		lblNewLabel_1_1.setBounds(30, 127, 99, 34);
-		dangnhap.add(lblNewLabel_1_1);
-		btndn = new JButton("Đăng nhập");
-		btndn.setSelectedIcon(new ImageIcon("E:\\APP_quanLyLuong\\App_QuanlyluongSP\\src\\Icons\\baseline_login_black_24dp.png"));
-		btndn.setFont(new Font("Tahoma", Font.BOLD, 12));
-		btndn.setBackground(new Color(255, 228, 196));
-		btndn.setBounds(142, 235, 146, 34);
-		 btndn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		dangnhap.add(btndn);
-		
-		pass = new JPasswordField();
-		pass.setBounds(155, 127, 213, 33);
-		dangnhap.add(pass);
-		
-		 jcbxem = new JCheckBox("Xem mật khẩu");
-		jcbxem.setBounds(153, 177, 124, 23);
-		dangnhap.add(jcbxem);
-		jcbxem.addItemListener(new ItemListener() {
-			
+		setTitle("Đăng nhập");
+
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 11, 729, 410);
+		contentPane.add(panel);
+		panel.setLayout(null);
+
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(-11, -11, 395, 421);
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+
+		JLabel lblLogo = new JLabel("New label");
+		ImageLogo = new ImageIcon(getClass().getResource("/Icons/logo/logo.png"));
+		Image scaleLogo = ImageLogo.getImage().getScaledInstance(395, 421, Image.SCALE_SMOOTH);
+		ImageLogo = new ImageIcon(scaleLogo);
+		lblLogo.setIcon(ImageLogo);
+		lblLogo.setBounds(0, 0, 405, 421);
+
+		panel_1.add(lblLogo);
+
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, new Color(0, 255, 102), null, null, null));
+		panel_2.setBackground(new Color(0, 0, 140));
+		panel_2.setForeground(new Color(255, 255, 255));
+		panel_2.setBounds(382, 0, 368, 410);
+		panel.add(panel_2);
+		panel_2.setLayout(null);
+
+		JLabel lblWelcome = new JLabel("Xin Chào!");
+		lblWelcome.setForeground(new Color(239, 239, 239));
+		lblWelcome.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD | Font.ITALIC, 22));
+		lblWelcome.setHorizontalAlignment(SwingConstants.CENTER);
+		lblWelcome.setBounds(79, 39, 215, 29);
+		panel_2.add(lblWelcome);
+
+		textFieldUsername = new JTextField();
+		textFieldUsername.setBackground(new Color(255, 255, 255));
+		textFieldUsername.setForeground(new Color(0, 0, 0));
+		textFieldUsername.setToolTipText("Username:\r\n");
+		textFieldUsername.setBounds(79, 164, 234, 29);
+		panel_2.add(textFieldUsername);
+		textFieldUsername.setColumns(10);
+
+		JLabel lblngNhpTi = new JLabel("Đăng nhập tài khoản");
+		lblngNhpTi.setHorizontalAlignment(SwingConstants.CENTER);
+		lblngNhpTi.setForeground(new Color(216, 216, 216));
+		lblngNhpTi.setFont(new Font("Verdana", Font.BOLD, 24));
+		lblngNhpTi.setBounds(35, 79, 294, 29);
+		panel_2.add(lblngNhpTi);
+
+		JLabel lblMaNhanVien = new JLabel("Mã nhân viên:");
+		lblMaNhanVien.setFont(new Font("Verdana", Font.BOLD, 14));
+		lblMaNhanVien.setForeground(new Color(239, 239, 239));
+		lblMaNhanVien.setBounds(51, 139, 169, 14);
+		panel_2.add(lblMaNhanVien);
+
+		JLabel lblPassword = new JLabel("Mật khẩu:");
+		lblPassword.setForeground(new Color(239, 239, 239));
+		lblPassword.setFont(new Font("Verdana", Font.BOLD, 14));
+		lblPassword.setBounds(50, 220, 98, 14);
+		panel_2.add(lblPassword);
+
+		passwordField = new JPasswordField();
+		passwordField.setBackground(new Color(255, 255, 255));
+		passwordField.setBounds(79, 245, 234, 29);
+		panel_2.add(passwordField);
+
+		JLabel lblImgUser = new JLabel("");
+		ImageIcon ImageUser = new ImageIcon(getClass().getResource("/Icons/logo/Username.png"));
+		Image scaleUser = ImageUser.getImage().getScaledInstance(90, 90, Image.SCALE_SMOOTH);
+		ImageUser = new ImageIcon(scaleUser);
+		lblImgUser.setIcon(ImageUser);
+		lblImgUser.setBounds(0, 164, 61, 29);
+		panel_2.add(lblImgUser);
+
+		JLabel lblImgPassword = new JLabel("");
+		ImageIcon ImagePassword = new ImageIcon(getClass().getResource("/Icons/logo/LogPassword.png"));
+		Image scalePW = ImagePassword.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+		ImagePassword = new ImageIcon(scalePW);
+		lblImgPassword.setIcon(ImagePassword);
+		lblImgPassword.setBounds(35, 245, 61, 29);
+		panel_2.add(lblImgPassword);
+
+		JButton btnQuenMK = new JButton("Quên mật khẩu?");
+		btnQuenMK.setForeground(new Color(255, 255, 255));
+		btnQuenMK.setBackground(new Color(0, 0, 151));
+		btnQuenMK.setFont(new Font("Tahoma", Font.ITALIC, 11));
+		btnQuenMK.setBounds(196, 298, 117, 23);
+		panel_2.add(btnQuenMK);
+
+		btnLogin = new JButton("Đăng nhập");
+
+		JCheckBox chckbxXem = new JCheckBox("Xem mật khẩu");
+		chckbxXem.setForeground(new Color(255, 255, 255));
+		chckbxXem.setBackground(new Color(0, 0, 151));
+		chckbxXem.setFont(new Font("Tahoma", Font.ITALIC, 11));
+		chckbxXem.setBounds(79, 298, 97, 23);
+		panel_2.add(chckbxXem);
+
+		chckbxXem.addItemListener(new ItemListener() {
+
 			@Override
 			public void itemStateChanged(ItemEvent e) {
 				// TODO Auto-generated method stub
-				if(jcbxem.isSelected())
-				{
-					pass.setEchoChar((char)0);
-				}
-				else
-					pass.setEchoChar('*');
-				
+				if (chckbxXem.isSelected()) {
+					passwordField.setEchoChar((char) 0);
+				} else
+					passwordField.setEchoChar('*');
+
 			}
 		});
-		btndn.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
-		
-		btndn.addActionListener(this);
-		btndn.setMnemonic(KeyEvent.VK_ENTER);
-		
-		 lblexit = new JLabel("");
-		lblexit.setIcon(new ImageIcon("E:\\APP_quanLyLuong\\App_QuanlyluongSP\\src\\Icons\\btn-close.png"));
-		lblexit.setBounds(402, 11, 38, 23);
-		lblexit.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		dangnhap.add(lblexit);
-		lblexit.addMouseListener(new MouseListener() {
-	         @Override
-	         public void mouseClicked(MouseEvent e) {
-	            System.exit(0);
-	         }
 
-	         @Override
-	         public void mousePressed(MouseEvent e) {
-	        	
-	         }
-	         
+		btnLogin.setForeground(new Color(255, 255, 255));
+		btnLogin.setBackground(new Color(0, 0, 72));
+		btnLogin.setFont(new Font("Tahoma", Font.BOLD, 18));
+		btnLogin.setBounds(115, 349, 140, 34);
+		panel_2.add(btnLogin);
 
-	         @Override
-	         public void mouseReleased(MouseEvent e) {
-//	        	 lblexit.setIcon(new ImageIcon("Icons/btn-close.png"));
-	         }
+		btnLogin.addActionListener(this);
+		btnLogin.setMnemonic(KeyEvent.VK_ENTER);
 
-	         @Override
-	         public void mouseEntered(MouseEvent e) {
-//	             lblexit.setIcon(new ImageIcon("Icons/btn-close--hover.png"));
-	         }
-
-	         @Override
-	         public void mouseExited(MouseEvent e) {
-//	           lblexit.setIcon(new ImageIcon("Icons/btn-close.png"));
-	         }
-	     });
-		
-		
 	}
-	 
-	
-	
-	
-	
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		Object o=e.getSource();
-		if(o.equals(btndn)) {
-			char[] password = pass.getPassword();
+		Object o = e.getSource();
+		if (o.equals(btnLogin)) {
+			char[] password = passwordField.getPassword();
 			String passwordString = new String(password);
-			if(txtma.getText().equals("NV123")&&passwordString.equals("123")) {
-				JOptionPane.showMessageDialog( this,"Đăng nhập thành công!!!<");
-			 new App_main().setVisible(true);
-			dispose();
+			if (textFieldUsername.getText().equals("NV123") && passwordString.equals("123")) {
+				JOptionPane.showMessageDialog(this, "Đăng nhập thành công!!!");
+				new App_main().setVisible(true);
+				dispose();
+			} else {
+				JOptionPane.showMessageDialog(this, "Mã nhân viên hay mật khẩu sai,yêu cầu nhập lại!!");
+				textFieldUsername.setText("");
+				passwordField.setText("");
+				textFieldUsername.requestFocus();
 			}
-			else
-			{
-				JOptionPane.showMessageDialog( this,"Mã nhân viên hay mật khẩu sai,yêu cầu nhập lại!!");
-				txtma.setText("");
-				pass.setText("");
-				txtma.requestFocus();
-			}
-				
-			
+
 		}
-		
+
 	}
 }
